7d3f17afd51b2ea2afff496c48922618b78fbd21
Sergey <sergeykinyak@gmail.com>
2024-01-10 21:13:11 +0200

    calc checkbox

---
+++ components/button/CalcButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="calc_button">
+    <button>
+      <slot></slot>
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Calc-Button",
+};
+</script>
+<style scoped>
+.calc_button button {
+  width: 300px;
+  font-size: 16px;
+  line-height: 25px;
+  padding: 9px 0px;
+  background: transparent;
+  text-wrap: nowrap;
+  border: 1px solid rgba(234, 90, 37, 1);
+  color: rgb(255, 255, 255);
+  text-align: center;
+  margin: 5px 10px;
+}
+.calc_button button:hover {
+  background: rgba(234, 90, 37, 1);
+}
+.active {
+  background: rgba(234, 90, 37, 1);
+}
+@media (max-width: 767px) {
+  .calc_button button {
+    font-size: 12px;
+    line-height: 15px;
+    width: 170px;
+    height: 42px;
+    text-wrap: wrap;
+  }
+}
+@media (max-width: 590px) {
+  .calc_button button {
+    margin: 5px 0px;
+  }
+}
+</style>
--- components/main/Calc.vue
+++ components/main/Calc.vue
@@ -1,38 +1,189 @@
-<template >
-	<div class="calc">
-		<div class="container">
-			<div class="calc_title">
-				<h2>КАЛЬКУЛЯТОР <span>ВАРТОСТІ </span></h2>
-			</div>
-			<div class="calc_tabs"></div>
-			<div class="calc_body">
+<template>
+  <div class="calc">
+    <div class="container">
+      <div class="calc_title">
+        <h2>КАЛЬКУЛЯТОР <span>ВАРТОСТІ </span></h2>
+      </div>
+      <div class="calc_subtitle">
+        <p>Розрахуйте орієнтовну вартість робіт</p>
+        <span>Сбросить</span>
+      </div>
+      <div class="calc_tabs">
+        <CalcButton
+          v-for="service in itemListServices"
+          :service="service"
+          :key="service[i]"
+          >{{ service }}</CalcButton
+        >
+      </div>
+      <div class="calc_body">
+        <div class="calc_body__title">
+          <h4>РОЗРОБКА САЙТУ:</h4>
+        </div>
+        <div class="calc_body__checkbox">
+          <div class="calc_body__checkbox_item">
 
-			</div>
-
-			<div class="calc_footer">
-				<MainButton>Заказати</MainButton>
-			</div>
-		</div>
-	</div>
+            <input type="checkbox" id="cmsCheckbox" name="cmsCheckbox" checked />
+				<span></span>
+            <label for="cmsCheckbox">Рукописна CMS</label>
+          </div>
+			 <div class="calc_body__checkbox_item">
+            <input type="checkbox" id="formCheckbox" name="formCheckbox" checked />
+            <label for="formCheckbox">Форма збору даних</label>
+          </div>
+			 <div class="calc_body__checkbox_item">
+            <input type="checkbox" id="calcCheckbox" name="calcCheckbox" checked />
+            <label for="calcCheckbox">Калькулятор цін</label>
+          </div>
+        </div>
+        <div class="calc_body__footer">
+          <div class="calc_body__footer_column">
+            <div class="calc_body__footer_item">Орієнтовна вартість</div>
+            <div class="calc_body__footer_item">Від 0 грн</div>
+          </div>
+          <div class="calc_body__footer_column">
+            <div class="calc_body__footer_item">Термін виконання</div>
+            <div class="calc_body__footer_item">Від 0 днів</div>
+          </div>
+        </div>
+      </div>
+      <div class="calc_footer">
+        <MainButton>Заказати</MainButton>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-import MainButton from '../button/MainButton.vue';
+import MainButton from "../button/MainButton.vue";
+import CalcButton from "../button/CalcButton.vue";
 export default {
-	components:{MainButton},
-	name:"Calc"
-	
-}
+  components: { MainButton, CalcButton },
+  name: "Calc",
+  data() {
+    return {
+      itemListServices: [
+        "Розробка сайту",
+        "Розробка Landing page",
+        "Розробка Iнтернет магазину",
+        "Розробка портала",
+        "Контекстна реклама",
+        "SEO просування",
+        "Розробка логотипу",
+        "Розробка фiрмового стилю",
+      ],
+    };
+  },
+};
 </script>
 <style scoped>
-	.calc {
-		padding:0px 14.5px 100px 14.5px;
+.calc_body__checkbox {
+	display: flex;
+	flex-direction: column;
+}
+.calc_body__checkbox_item {
+	display: block;
+max-width: 200px;
+border: 1px solid rgba(220, 223, 230, 1);
+padding: 13px 34px 13px 11px;
+margin-bottom: 15px;
+text-align: flex-start;
+}
+.calc_body__checkbox_item input{
+	display: none;
+}
+.calc_body__checkbox_item span{
+
+}
+.calc_body__checkbox_item span {/* <-- стилизируем новый */
+height: 14px;
+  width: 14px;
+  display: inline-block;
+  position: relative;
+  background-color:#fcfcfc;
+  margin: 0px 0px -3px 0px;
+}
+/* .calc_body__checkbox_item span:hover{
+	background-color: rgba(234, 90, 37, 1);;
+} */
+.calc_body__checkbox_item span::before,
+.calc_body__checkbox_item span::after {
+	display: none;
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 1.5px;
+  width: 8px;
+  height: 1px;
+  background-color: rgb(252, 2, 2);
+}
+.calc_body__checkbox_item span::before{
+	width: 5px;
+	transform:  rotate(-135deg);
+}
+.calc_body__checkbox_item span::after {
+	top: 6px;
+	left: 4.5px;
+	transform:  rotate(135deg);
+}
+
+input[type=checkbox]:checked + .calc_body__checkbox_item span{
+	background: rgb(252, 2, 2);}
+	/* }{<-- ставим иконку, когда чекбокс включен 
+	.calc_body__checkbox_item span{
+		background-color: rgba(234, 90, 37, 1);
+	}
+	span:before,  span:after{
+display: block;
+	}} */
+.calc_body__checkbox_item label{
+	font-family: var(--classic);
+	color: rgba(248, 248, 248, 1);
+	opacity: 50%;
+	font-size: 12px;
+	text-wrap: nowrap;
+
+	margin-left: 8px;
+
+}
+.calc_body__title {
+  font-family: var(--title-font);
+  font-size: 42px;
+  line-height: 58px;
+}
+.calc_body__footer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px -10px 0px -10px;
+  margin: 0px -5px 0px -5px;
+}
+.calc_body__footer_column {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 0 1 50%;
+  margin: 0px 5px 0px 5px;
+}
+.calc_body__footer_item {
+  width: 100%;
+  color: #ffffff;
+  font-family: var(--title);
+  align-items: flex-start;
+  background: rgba(248, 248, 248, 0.05);
+  margin: 5px 5px;
+  padding: 14px 15px;
+}
+.calc {
+  padding: 0px 14.5px 100px 14.5px;
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
 }
 .container {
-	max-width: 1280px;
+  max-width: 1280px;
   margin: 0 auto;
 }
 
@@ -46,18 +197,109 @@ export default {
   text-align: flex-start;
 }
 @supports (-webkit-text-stroke: 1px white) {
-	.calc_title span {
+  .calc_title span {
     text-shadow: none;
     color: transparent;
     -webkit-text-stroke: 1px #ffffff;
     position: relative;
   }
 }
+.calc_subtitle {
+  width: 100%;
+  font-size: 12px;
+  color: rgba(248, 248, 248, 1);
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+}
+.calc_subtitle p {
+  font-family: var(--subTitle-font);
+  text-transform: uppercase;
+}
+.calc_subtitle span {
+  display: inline-block;
+  position: relative;
+  opacity: 50%;
+  font-size: 16px;
+  margin-bottom: 6px;
+}
+.calc_subtitle span::before,
+.calc_subtitle span::after {
+  content: "";
+  position: absolute;
+  top: 40%;
+  left: -20px;
+  width: 18px;
+  height: 2px;
+  background: rgb(184, 183, 183);
+}
+
+.calc_subtitle span::after {
+  transform: rotate(45deg);
+}
+.calc_subtitle span::before {
+  transform: rotate(135deg);
+}
+.calc_subtitle span:hover {
+  opacity: 100%;
+}
+
 .calc_tabs {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 40px;
 }
 .calc_body {
 }
 .calc_footer {
-	max-width: 328px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding-top: 40px;
+}
+.calc_footer > * {
+  width: 170px;
+}
+@media (max-width: 767px) {
+  .calc {
+    padding: 0px 14.5px 60px 14.5px;
+  }
+  .calc_body__title {
+    font-family: var(--classic);
+    font-size: 22px;
+    line-height: 21px;
+  }
+  .calc_title {
+    font-family: var(--classic);
+
+    line-height: 21px;
+    font-size: 22px;
+    margin-bottom: 20px;
+    text-align: flex-start;
+  }
+  .calc_subtitle {
+    margin-bottom: 40px;
+  }
+  .calc_body__footer_item {
+    font-size: 12px;
+    line-height: 25px;
+  }
+  .calc_subtitle p {
+    font-size: 10px;
+  }
+  .calc_subtitle span {
+    display: none;
+  }
+  .calc_tabs {
+    width: 100%;
+    height: 100%;
+    justify-content: space-between;
+    margin-bottom: 40px;
+    padding: 0px -10px 0px -10px;
+  }
 }
 </style>
