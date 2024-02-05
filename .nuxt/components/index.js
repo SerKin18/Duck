cf48e07f603fbb0872c9ca244c58b621dfa58ada
SerKin18 <kinyaksereja@gmail.com>
2024-02-05 21:57:42 +0200

    buld

---
+++ .nuxt/components/index.js
@@ -0,0 +1,66 @@
+export { default as App } from '../..\\components\\App.vue'
+export { default as ButtonCalcButton } from '../..\\components\\button\\CalcButton.vue'
+export { default as ButtonClassicButton } from '../..\\components\\button\\ClassicButton.vue'
+export { default as ButtonMainButton } from '../..\\components\\button\\MainButton.vue'
+export { default as ButtonMainButtonBlack } from '../..\\components\\button\\MainButtonBlack.vue'
+export { default as ButtonServiceButton } from '../..\\components\\button\\ServiceButton.vue'
+export { default as ButtonStarsRating } from '../..\\components\\button\\StarsRating.vue'
+export { default as Header } from '../..\\components\\header\\Header.vue'
+export { default as Footer } from '../..\\components\\footer\\Footer.vue'
+export { default as FooterItem } from '../..\\components\\footer\\FooterItem.vue'
+export { default as MainBanner } from '../..\\components\\main\\Banner.vue'
+export { default as MainCalc } from '../..\\components\\main\\Calc.vue'
+export { default as MainCalcCheckboxItem } from '../..\\components\\main\\CalcCheckboxItem.vue'
+export { default as MainCalcCheckboxLineItem } from '../..\\components\\main\\CalcCheckboxLineItem.vue'
+export { default as MainCalckCheckBoxLine } from '../..\\components\\main\\CalckCheckBoxLine.vue'
+export { default as MainCalckCheckBoxLine2 } from '../..\\components\\main\\CalckCheckBoxLine2.vue'
+export { default as MainCalckCheckBoxLine3 } from '../..\\components\\main\\CalckCheckBoxLine3.vue'
+export { default as MainContacts } from '../..\\components\\main\\Contacts.vue'
+export { default as MainContactsBlock } from '../..\\components\\main\\ContactsBlock.vue'
+export { default as MainFQS } from '../..\\components\\main\\FQS.vue'
+export { default as MainFQSItem } from '../..\\components\\main\\FQSItem.vue'
+export { default as Main } from '../..\\components\\main\\Main.vue'
+export { default as MainModal } from '../..\\components\\main\\Modal.vue'
+export { default as MainModalOrder } from '../..\\components\\main\\ModalOrder.vue'
+export { default as MainPortfolio } from '../..\\components\\main\\Portfolio.vue'
+export { default as MainPortfolioItem } from '../..\\components\\main\\PortfolioItem.vue'
+export { default as MainRadio } from '../..\\components\\main\\Radio.vue'
+export { default as MainRadioItem } from '../..\\components\\main\\RadioItem.vue'
+export { default as MainReviews } from '../..\\components\\main\\Reviews.vue'
+export { default as MainReviewsItem } from '../..\\components\\main\\ReviewsItem.vue'
+export { default as MainService } from '../..\\components\\main\\Service.vue'
+export { default as MainServiceItem } from '../..\\components\\main\\ServiceItem.vue'
+export { default as MainTickers } from '../..\\components\\main\\Tickers.vue'
+export { default as MainWhyUs } from '../..\\components\\main\\WhyUs.vue'
+export { default as MainWhyUsItem } from '../..\\components\\main\\WhyUsItem.vue'
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
---
+++ .nuxt/components/readme.md
@@ -0,0 +1,43 @@
+# Discovered Components
+
+This is an auto-generated list of components discovered by [nuxt/components](https://github.com/nuxt/components).
+
+You can directly use them in pages and other components without the need to import them.
+
+**Tip:** If a component is conditionally rendered with `v-if` and is big, it is better to use `Lazy` or `lazy-` prefix to lazy load.
+
+- `<App>` | `<app>` (components/App.vue)
+- `<ButtonCalcButton>` | `<button-calc-button>` (components/button/CalcButton.vue)
+- `<ButtonClassicButton>` | `<button-classic-button>` (components/button/ClassicButton.vue)
+- `<ButtonMainButton>` | `<button-main-button>` (components/button/MainButton.vue)
+- `<ButtonMainButtonBlack>` | `<button-main-button-black>` (components/button/MainButtonBlack.vue)
+- `<ButtonServiceButton>` | `<button-service-button>` (components/button/ServiceButton.vue)
+- `<ButtonStarsRating>` | `<button-stars-rating>` (components/button/StarsRating.vue)
+- `<Header>` | `<header>` (components/header/Header.vue)
+- `<Footer>` | `<footer>` (components/footer/Footer.vue)
+- `<FooterItem>` | `<footer-item>` (components/footer/FooterItem.vue)
+- `<MainBanner>` | `<main-banner>` (components/main/Banner.vue)
+- `<MainCalc>` | `<main-calc>` (components/main/Calc.vue)
+- `<MainCalcCheckboxItem>` | `<main-calc-checkbox-item>` (components/main/CalcCheckboxItem.vue)
+- `<MainCalcCheckboxLineItem>` | `<main-calc-checkbox-line-item>` (components/main/CalcCheckboxLineItem.vue)
+- `<MainCalckCheckBoxLine>` | `<main-calck-check-box-line>` (components/main/CalckCheckBoxLine.vue)
+- `<MainCalckCheckBoxLine2>` | `<main-calck-check-box-line2>` (components/main/CalckCheckBoxLine2.vue)
+- `<MainCalckCheckBoxLine3>` | `<main-calck-check-box-line3>` (components/main/CalckCheckBoxLine3.vue)
+- `<MainContacts>` | `<main-contacts>` (components/main/Contacts.vue)
+- `<MainContactsBlock>` | `<main-contacts-block>` (components/main/ContactsBlock.vue)
+- `<MainFQS>` | `<main-f-q-s>` (components/main/FQS.vue)
+- `<MainFQSItem>` | `<main-f-q-s-item>` (components/main/FQSItem.vue)
+- `<Main>` | `<main>` (components/main/Main.vue)
+- `<MainModal>` | `<main-modal>` (components/main/Modal.vue)
+- `<MainModalOrder>` | `<main-modal-order>` (components/main/ModalOrder.vue)
+- `<MainPortfolio>` | `<main-portfolio>` (components/main/Portfolio.vue)
+- `<MainPortfolioItem>` | `<main-portfolio-item>` (components/main/PortfolioItem.vue)
+- `<MainRadio>` | `<main-radio>` (components/main/Radio.vue)
+- `<MainRadioItem>` | `<main-radio-item>` (components/main/RadioItem.vue)
+- `<MainReviews>` | `<main-reviews>` (components/main/Reviews.vue)
+- `<MainReviewsItem>` | `<main-reviews-item>` (components/main/ReviewsItem.vue)
+- `<MainService>` | `<main-service>` (components/main/Service.vue)
+- `<MainServiceItem>` | `<main-service-item>` (components/main/ServiceItem.vue)
+- `<MainTickers>` | `<main-tickers>` (components/main/Tickers.vue)
+- `<MainWhyUs>` | `<main-why-us>` (components/main/WhyUs.vue)
+- `<MainWhyUsItem>` | `<main-why-us-item>` (components/main/WhyUsItem.vue)
